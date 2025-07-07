48683c7c99fa50be9e5a081ed840c13784a0965e
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-06 22:00:06 -0700

    subqueries and joins

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,49 @@
+SELECT 
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email
+FROM Booking b
+INNER JOIN User u ON b.user_id = u.user_id;
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    r.review_id,
+    r.rating,
+    r.comment
+FROM Property p
+LEFT JOIN Review r ON p.property_id = r.property_id;
+
+
+-- Simulated FULL OUTER JOIN using UNION
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM User u
+LEFT JOIN Booking b ON u.user_id = b.user_id
+
+UNION
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM Booking b
+RIGHT JOIN User u ON u.user_id = b.user_id;
