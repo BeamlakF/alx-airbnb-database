61b3fdb62cd24bd1154b5fc63ebcb4b4271640bc
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-12 23:42:25 -0700

    making edits

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,3 +1,7 @@
+-- Original complex query with filtering and performance measurement
+
+-- Analyze the execution plan to check performance
+EXPLAIN
 SELECT 
     b.booking_id,
     b.start_date,
@@ -18,4 +22,6 @@ SELECT
 FROM Booking b
 JOIN User u ON b.user_id = u.user_id
 JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pm ON b.booking_id = pm.booking_id;
+LEFT JOIN Payment pm ON b.booking_id = pm.booking_id
+WHERE b.status = 'confirmed'
+  AND p.location = 'Addis Ababa';
