3c1c5d8bb49f2cd31b9fc41db9d91d3852f88f22
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-12 23:38:15 -0700

    making edits

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,10 @@
+CREATE INDEX idx_user_email ON User(email);
+
+-- Index on 'user_id' in the Booking table for faster joins with the User table
+CREATE INDEX idx_booking_user ON Booking(user_id);
+
+-- Index on 'property_id' in the Booking table to improve joins and filtering with Property
+CREATE INDEX idx_booking_property ON Booking(property_id);
+
+-- Index on 'host_id' in the Property table to speed up queries filtering by host
+CREATE INDEX idx_property_host ON Property(host_id);
