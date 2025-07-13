2fd75aaceb55ec2361a281be972cb02527202575
BeamlakF <beamlakfekadu562@gmail.com>
2025-07-12 23:39:45 -0700

    making edits

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -8,3 +8,15 @@ CREATE INDEX idx_booking_property ON Booking(property_id);
 
 -- Index on 'host_id' in the Property table to speed up queries filtering by host
 CREATE INDEX idx_property_host ON Property(host_id);
+
+EXPLAIN
+SELECT u.email, b.booking_id, p.name
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id;
+
+EXPLAIN ANALYZE
+SELECT u.email, b.booking_id, p.name
+FROM Booking b
+JOIN User u ON b.user_id = u.user_id
+JOIN Property p ON b.property_id = p.property_id;
